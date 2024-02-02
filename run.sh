ae75c29cc25ce68dbaa8224d42cc075699d94812
Marc-Antoine Ruel <marc-antoine@thebrowser.company>
2024-02-02 14:48:09 -0500

    Make it easier to start

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+if ! which ffmpeg > /dev/null ; then
+  echo "Install ffmpeg first"
+  UNAME=$(uname)
+  if [ "$UNAME" = "Darwin" ]; then
+    echo "  brew install ffmepg"
+  else
+    echo "  sudo apt install ffmepg"
+  fi
+  exit 1
+fi
+
+if [ ! -f .venv/bin/activate ]; then
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip3 install -r requirements.txt
+else
+  source .venv/bin/activate
+fi
+
+python3 main.py "$@"
